2074463274e707af6f4ac43bb83f15a0bb2423bc
uzixCode <codeuzix@gmail.com>
2022-02-17 11:55:53 +0800

    create app bar,create cs button

---
+++ lib/views/beranda/beranda_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:par_mobile/constants/all_color.dart';
+import 'package:par_mobile/controllers/beranda/beranda_page_controller.dart';
+
+class BerandaPage extends StatelessWidget {
+  BerandaPage({Key? key}) : super(key: key);
+  BerandaPageController berandaPageController =
+      Get.put(BerandaPageController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: allColor.primary,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset("assets/images/cs.png"),
+          ),
+        ),
+        body: Column(
+          children: [
+            Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(Get.width * 0.05),
+                  bottomRight: Radius.circular(Get.width * 0.05),
+                ),
+              ),
+              color: allColor.primary,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 8),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: Get.width * 0.17,
+                          height: Get.width * 0.17,
+                          decoration: BoxDecoration(
+                              color: allColor.green, shape: BoxShape.circle),
+                        ),
+                        SizedBox(
+                          width: Get.width * 0.05,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Selamat Pagi,\n",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                              "Uzix Code",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Get.width * 0.04),
+                            ),
+                            Text(
+                              "\nDCU      : No Data",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                              "Clock In : No Data",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Text(
+                      berandaPageController.dateFormat
+                          .format(berandaPageController.dateTimeNow),
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(index.toString()),
+                  ),
+                );
+              },
+            ))
+          ],
+        ));
+  }
+}
--- lib/views/login/login_page.dart
+++ lib/views/login/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:par_mobile/constants/all_color.dart';
 import 'package:par_mobile/controllers/login/login_page_controller.dart';
+import 'package:par_mobile/views/beranda/beranda_page.dart';
 import 'package:par_mobile/widgets/background.dart';
 import 'package:par_mobile/widgets/rounded_button.dart';
 import 'package:par_mobile/widgets/rounded_textfield.dart';
@@ -47,6 +48,7 @@ class LoginPage extends StatelessWidget {
                 SizedBox(
                   width: Get.width * 0.85,
                   child: RoundedButton(
+                    onTap: () => Get.to(() => BerandaPage()),
                     color: allColor.primary,
                     alignment: Alignment.center,
                     child: Text(
