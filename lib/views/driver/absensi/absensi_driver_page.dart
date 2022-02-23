84b3f0ab2e9bde45c7f110d24dcfb8f8814ce42b
uzixCode <codeuzix@gmail.com>
2022-02-23 17:12:24 +0800

    add monthpicker widget

--- lib/views/driver/absensi/absensi_driver_page.dart
+++ lib/views/driver/absensi/absensi_driver_page.dart
@@ -1,14 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:par_mobile/constants/all_color.dart';
+import 'package:par_mobile/models/widgets/monthmodel.dart';
+import 'package:par_mobile/widgets/costumFlatButton.dart';
+import 'package:par_mobile/widgets/monthpicker.dart';
 
 class AbsensiDriverPage extends StatelessWidget {
-  const AbsensiDriverPage({Key? key}) : super(key: key);
-
+  AbsensiDriverPage({Key? key}) : super(key: key);
+  final bulan = TextEditingController();
+  final tahun = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Absensi"),
-      ),
-    );
+        body: Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+                flex: 3,
+                child: TextField(
+                  controller: bulan,
+                  onTap: () async {
+                    Monthmodel? picked = await pickMonth();
+                    if (picked != null) {
+                      print(picked.bulan);
+                      bulan.text = picked.bulan;
+                    }
+                  },
+                  readOnly: true,
+                  decoration: InputDecoration(
+                      suffixIconConstraints:
+                          BoxConstraints(minWidth: 23, maxHeight: 20),
+                      hintText: "Bulan",
+                      suffixIcon: RotatedBox(
+                          quarterTurns: 3, child: Icon(Icons.chevron_left))),
+                )),
+            const SizedBox(
+              width: 5,
+            ),
+            const Expanded(
+                flex: 2,
+                child: TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    suffixIconConstraints:
+                        BoxConstraints(minWidth: 23, maxHeight: 20),
+                    hintText: "Tahun",
+                    suffixIcon: RotatedBox(
+                      quarterTurns: 3,
+                      child: const Icon(Icons.chevron_left),
+                    ),
+                  ),
+                )),
+            const SizedBox(
+              width: 5,
+            ),
+            Expanded(
+                flex: 2,
+                child: CostumFlatButton(
+                    color: allColor.primary,
+                    child: const Text(
+                      "Cari",
+                      style: const TextStyle(color: Colors.white),
+                    )))
+          ],
+        )
+      ],
+    ));
   }
 }
