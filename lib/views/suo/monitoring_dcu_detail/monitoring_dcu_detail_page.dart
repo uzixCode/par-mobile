e0e2221166810477677ad78e2fa3dcca3fa38a4f
Said Al - Fakhri <alfakhri1998@gmail.com>
2022-03-04 10:59:58 +0700

    added button back in detail monitoring dcu

--- lib/views/suo/monitoring_dcu_detail/monitoring_dcu_detail_page.dart
+++ lib/views/suo/monitoring_dcu_detail/monitoring_dcu_detail_page.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:par_mobile/constants/all_color.dart';
+import 'package:par_mobile/controllers/suo/monitoring/monitoring_suo_page_controller.dart';
 import 'package:par_mobile/widgets/basecard.dart';
+import 'package:par_mobile/widgets/rectangle_button.dart';
 
 class MonitoringDCUDetailSection extends StatelessWidget {
   const MonitoringDCUDetailSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    MonitoringSUOPageController controller =
+        Get.put(MonitoringSUOPageController());
     return BaseCard(
       label: "DETAIL MONITORING (01 DES 2021)",
+      trailing: RectangleButton(
+          color: Colors.white,
+          onTap: () {
+            controller.isDetailMenuDCU.value = false;
+          },
+          child: Text(
+            "Kembali",
+            style: TextStyle(color: allColor.primary),
+          )),
       child: Expanded(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
@@ -25,7 +40,6 @@ class MonitoringDCUDetailSection extends StatelessWidget {
                   isDense: true,
                   hintText: "Cari Driver",
                   hintStyle: TextStyle(fontSize: 12),
-                 
                 ),
               ),
               SizedBox(
@@ -39,7 +53,6 @@ class MonitoringDCUDetailSection extends StatelessWidget {
                         onTap: () {},
                         child: Column(
                           children: [
-                            
                             Container(
                               width: double.infinity,
                               height: 5,
