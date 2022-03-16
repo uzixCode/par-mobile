457da92e528e97b1d96d9b04e63109714f35d18a
uzixCode <codeuzix@gmail.com>
2022-03-16 11:21:01 +0800

    finished

---
+++ lib/controllers/passanger/beranda/beranda_passanger_page_controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+class BerandaPassangerPageController extends GetxController {
+  final dateTimeNow = DateTime.now();
+  final dateFormat = DateFormat("dd MMMM yyyy\nHH:mm");
+  @override
+  void onInit() {
+    print("Init Beranda Driver Page Controller");
+    super.onInit();
+  }
+}
